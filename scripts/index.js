8100908bf3b96da19a0e402f223ee4c1b32d6ee0
FOXdesign <contact@foxdesign.ie>
2020-12-09 12:55:28 +0000

    first

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+
+// slider img
+let slideIndex = 1;
+showImg(slideIndex);
+
+function plusDivs(n) {
+    showImg(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showImg(slideIndex = n);
+}
+
+function showImg(n) {
+    let i;
+    const x = document.getElementsByClassName("mySlides");
+    const dots = document.getElementsByClassName("dot");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("dot-active", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " dot-active";
+
+}
